e46b0500bd247db16d7eeacfb5aa7dbf83906b3d
Alireza <a.nakhaei@gmail.com>
2017-11-24 18:59:12 -0800

    initial version of pid

--- src/PID.cpp
+++ src/PID.cpp
@@ -6,16 +6,28 @@ using namespace std;
 * TODO: Complete the PID class.
 */
 
-PID::PID() {}
+PID::PID() {
+    d_error=0;
+    p_error=0;
+    i_error=0;
+}
 
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  Kp=Kp;
+  Ki=ki;
+  Kd=kd;
 }
 
 void PID::UpdateError(double cte) {
+    d_error = p_error-cte;
+    p_error=cte;
+    i_error +=cte;
+
 }
 
 double PID::TotalError() {
+    return -kp * p_error - kd * d_error - ki * i_error;
 }
 
